33bf4a1dc640438da6b075d3e1c846c8ae808d2d
lxjm23 <78394507+lxjm23@users.noreply.github.com>
2021-11-04 17:46:34 -0400

    refactored UserController.java

--- recipe/recipe-web/src/main/java/ca/gbc/recipe/controllers/UserController.java
+++ recipe/recipe-web/src/main/java/ca/gbc/recipe/controllers/UserController.java
@@ -31,8 +31,14 @@ public class UserController {
     }
 
     @RequestMapping(value = "/registered")
-    public String submitForm(@ModelAttribute("user") User user){
+    public String success(@ModelAttribute("user") User user){
         userService.save(user);
+        /*
+        * database codes here
+        *
+        * */
+
+        //prints data just to check if this function is working
         System.out.println(user.getFirstname());
         System.out.println(user.getLastname());
         System.out.println(user.getUsername());
