c636db41f60f0c9d4f369ae9fe2b3039a252fa4c
mr-trespeces <mr.trespeces@gmail.com>
2021-11-06 20:26:03 -0400

    updated Recipe

--- recipe/recipe-data/src/main/java/ca/gbc/recipe/services/RecipeService.java
+++ recipe/recipe-data/src/main/java/ca/gbc/recipe/services/RecipeService.java
@@ -1,11 +1,24 @@
 package ca.gbc.recipe.services;
 
 import ca.gbc.recipe.model.Recipe;
+import ca.gbc.recipe.repository.RecipeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
 
 @Service
 public class RecipeService {
+
+    @Autowired
+    private RecipeRepository repo;
+
+    public void save(Recipe object) {
+        repo.save(object);
+    }
+
+    public List<Recipe> findByName(String name) {
+        return findByName(name);
+    }
 
 }
