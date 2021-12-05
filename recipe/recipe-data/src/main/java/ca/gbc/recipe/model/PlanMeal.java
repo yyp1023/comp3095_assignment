2c5b2d10c494c6bee193d452f3f960dbd0d31851
mr-trespeces <mr.trespeces@gmail.com>
2021-12-05 03:03:25 -0500

    Meal Event Update && Delete Complete

--- recipe/recipe-data/src/main/java/ca/gbc/recipe/model/PlanMeal.java
+++ recipe/recipe-data/src/main/java/ca/gbc/recipe/model/PlanMeal.java
@@ -43,7 +43,7 @@ public class PlanMeal extends BaseEntity {
     }
 
     @Column(name = "date_created")
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
     private Date planned_date;
 
 
@@ -64,9 +64,44 @@ public class PlanMeal extends BaseEntity {
         this.user_selected_recipe = user_selected_recipe;
     }
 
+    public String viewShortMonth()
+    {
+        switch (planned_date.getMonth() + 1)
+        {
+            case 1:
+                return "Jan";
+            case 2:
+                return "Feb";
+            case 3:
+                return "Mar";
+            case 4:
+                return "Apr";
+            case 5:
+                return "May";
+            case 6:
+                return "Jun";
+            case 7:
+                return "July";
+            case 8:
+                return "Aug";
+            case 9:
+                return "Sep";
+            case 10:
+                return "Oct";
+            case 11:
+                return "Nov";
+            case 12:
+                return "Dec";
+            default:
+                return "-";
+        }
+    }
+
     public Date getPlanned_date() {
         return planned_date;
     }
+
+
 
     public void setPlanned_date(Date planned_date) {
         this.planned_date = planned_date;
--- recipe/recipe-web/src/main/java/ca/gbc/recipe/controllers/PlanMealController.java
+++ recipe/recipe-web/src/main/java/ca/gbc/recipe/controllers/PlanMealController.java
@@ -15,17 +15,22 @@
 package ca.gbc.recipe.controllers;
 
 import ca.gbc.recipe.model.PlanMeal;
+import ca.gbc.recipe.model.Recipe;
 import ca.gbc.recipe.model.User;
 import ca.gbc.recipe.repository.PlanMealRepository;
 import ca.gbc.recipe.repository.RecipeRepository;
 import ca.gbc.recipe.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.util.Date;
 
 @RequestMapping("/users/plan_meal")
 @Controller
@@ -47,8 +52,9 @@ public class PlanMealController {
     }
 
     @PostMapping(value= "/createPlan")
-    public String planMealCreated(@ModelAttribute("planMeal") PlanMeal planMeal){
+    public String planMealCreated(@ModelAttribute("planMeal") PlanMeal planMeal, Model model){
         planMealService.save(planMeal);
+
         return  "redirect:/users/plan_meal/index";
     }
 
@@ -58,9 +64,38 @@ public class PlanMealController {
         PlanMeal planmeal = new PlanMeal();
         planmeal.setUser_meal(userService.findUserById(user.getId()));
         model.addAttribute(planmeal);
-        model.addAttribute("localDate", LocalDate.now());
         model.addAttribute("recipes", recipeService.findAll());
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm");
+        Date date = new Date();
+        String sdate = formatter.format(date);
+        model.addAttribute("sdate", sdate );
         return "/plan_meals/createPlan";
     }
 
+    @RequestMapping(value = "/delete/{mealId}", method= RequestMethod.POST)
+    public String success(@PathVariable(name="mealId") Long mealId){
+        planMealService.delete(planMealService.getById(mealId));
+        return "redirect:/users/plan_meal/index";
+    }
+
+    @RequestMapping(value= "/update/{mealId}")
+    public String planMealUpdate(@PathVariable(name="mealId") Long mealId, HttpSession session, Model model){
+        PlanMeal planMeal = planMealService.getById(mealId);
+        model.addAttribute("planMeal", planMeal);
+        model.addAttribute("recipes", recipeService.findAll());
+        return  "/plan_meals/update";
+    }
+
+    @PostMapping(value = "/update")
+    public String mealUpdate(@Param("planMeal_id") Long planMeal_id, @Param("user_selected_recipe") String user_selected_recipe,
+                             @Param("planned_date") String planned_date) throws ParseException {
+        PlanMeal planMeal = planMealService.getById(planMeal_id);
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm");
+        Date date = formatter.parse(planned_date);
+        planMeal.setPlanned_date(date);
+        Recipe recipe = recipeService.getById(Long.parseLong(user_selected_recipe));
+        planMeal.setUser_selected_recipe(recipe);
+        planMealService.save(planMeal);
+        return "redirect:/users/plan_meal/index";
+    }
 }
