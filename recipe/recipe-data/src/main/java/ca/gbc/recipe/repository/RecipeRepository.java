26308af3870dfda7fd1fc216c87323d195b9dbe4
Kent <KentCodeToSchool@gmail.com>
2021-12-05 22:00:20 -0500

    Yey

--- recipe/recipe-data/src/main/java/ca/gbc/recipe/repository/RecipeRepository.java
+++ recipe/recipe-data/src/main/java/ca/gbc/recipe/repository/RecipeRepository.java
@@ -1,6 +1,7 @@
 package ca.gbc.recipe.repository;
 
 import ca.gbc.recipe.model.Recipe;
+import ca.gbc.recipe.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -22,7 +23,5 @@ public interface RecipeRepository extends JpaRepository<Recipe, Long> {
     @Query("SELECT r FROM Recipe r WHERE r.user_id =?1")
     public List<Recipe> findByUser_id(User user_id);
 
-    @Query("SELECT r FROM Recipe r WHERE r.status = FALSE ")
-    public List<Recipe> isPublic();
 
 }
